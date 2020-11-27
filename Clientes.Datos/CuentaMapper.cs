c771d2b95c5fb9bbc33856b6c0b5a17e47b5f3b0
Jeremias Hernandez <her_jere@hotmail.com>
2020-11-27 09:26:00 -0300

    1

---
+++ Clientes.Datos/CuentaMapper.cs
@@ -0,0 +1,41 @@
+﻿using Clientes.Entidades;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clientes.Datos
+{
+    public class CuentaMapper
+    {
+        public List<Cuenta> TraerCuentas()
+        {
+            string json = WebHelper.Get("/api/v1/cuenta");
+
+            List<Cuenta> cuentas = JsonConvert.DeserializeObject<List<Cuenta>>(json);
+
+            return cuentas;
+        }
+
+        public ResultadoTransaccion InsertarCuenta(Cuenta cuenta)
+        {
+            NameValueCollection n = new NameValueCollection();
+
+            n.Add("NroCuenta", cuenta.NroCuenta.ToString());
+            n.Add("Descripcion", cuenta.Desc);
+            n.Add("Saldo", cuenta.Saldo.ToString());
+            n.Add("FechaApertura", cuenta.FechaApertura);
+            n.Add("idCliente", cuenta.IdCliente.ToString());
+            n.Add("id", cuenta.Id.ToString());
+
+            string json = WebHelper.Post("/api/v1/cuenta", n);
+
+            ResultadoTransaccion res = JsonConvert.DeserializeObject<ResultadoTransaccion>(json);
+
+            return res;
+        }
+    }
+}
---
+++ Clientes.Entidades/Cuenta.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clientes.Entidades
+{
+    [DataContract]
+    public class Cuenta
+    {
+        public double _nroCuenta;
+        private string _desc;
+        private double _saldo;
+        private string _fechaApertura;
+        private int _idCliente;
+        private int _id;
+
+        [DataMember(Name ="nroCuenta")]
+        public double NroCuenta { get => _nroCuenta; set => _nroCuenta= value; }
+
+        [DataMember(Name = "descripcion")]
+        public string Desc { get => _desc; set => _desc = value; }
+
+        [DataMember(Name = "saldo")]
+        public double  Saldo { get => _saldo; set => _saldo = value; }
+
+        [DataMember(Name = "fechaApertura")]
+        public string FechaApertura { get => _fechaApertura; set => _fechaApertura = value; }
+
+        [DataMember(Name = "idCliente")]
+        public int IdCliente { get => _idCliente; set => _idCliente = value; }
+
+        [DataMember(Name = "id")]
+        public int Id { get => _id; set => _id = value; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} - Apertura: {1} - Saldo: {2}", this.NroCuenta, this.FechaApertura, this.Saldo) 
+        }
+
+
+    }
+}
